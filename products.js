0b1b3a75c0ab6ed0b7034329918ce1748911a839
Griffin <griffin@Griffins-MacBook-Air.local>
2017-11-27 13:52:22 -0600

    adding products

---
+++ products.js
@@ -0,0 +1,21 @@
+let classes = [
+  {
+  name: "Driver's Ed",
+  startdate: '12-1-17',
+  spots: 15,
+  price: 150
+  },
+  {
+  name: "Learner's Permit",
+  startdate: '12-15-17',
+  spots: 20,
+  price: 79.99
+  },
+  {
+    name: "Driver's Ed",
+    startdate: '12-1-17',
+    spots: 15,
+    price: 150 
+  }
+
+]
